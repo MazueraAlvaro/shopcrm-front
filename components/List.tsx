8b738ad008095b3cce5d70505910d750c173d348
Alvaro Mazuera U <me@alvaromazuera.com>
2022-05-26 11:04:07 -0500

    :green_heart: Fix linter problems and add eslint plugins

--- components/List.tsx
+++ components/List.tsx
@@ -1,19 +1,22 @@
-import * as React from 'react'
-import ListItem from './ListItem'
-import { User } from '../interfaces'
+import * as React from 'react';
+
+import { User } from '../interfaces';
+import ListItem from './ListItem';
 
 type Props = {
-  items: User[]
+  items: User[];
+};
+
+function List({ items }: Props) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          <ListItem data={item} />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-const List = ({ items }: Props) => (
-  <ul>
-    {items.map((item) => (
-      <li key={item.id}>
-        <ListItem data={item} />
-      </li>
-    ))}
-  </ul>
-)
-
-export default List
+export default List;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from 'react';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+import Link from 'next/link';
 
-export default IndexPage
+import Layout from '../components/Layout';
+
+function IndexPage() {
+  return (
+    <Layout title='Home | Next.js + TypeScript Example'>
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href='/about'>
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+}
+
+export default IndexPage;
