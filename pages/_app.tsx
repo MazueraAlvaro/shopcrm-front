7975f2c54561564057b510cb09327db28080332e
Alvaro Mazuera U <me@alvaromazuera.com>
2022-06-06 16:27:33 -0500

    :construction: Add template Ogani base files

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../public/styles/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../public/styles/font-awesome.min.css';
+import '../public/styles/elegant-icons.css';
+import '../public/styles/nice-select.css';
+// import '../public/styles/jquery-ui.min.css';
+import '../public/styles/owl.carousel.min.css';
+import '../public/styles/slicknav.min.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Link from 'next/link';
 
-import Layout from '../components/Layout';
+import Layout from '../components/layout/Default';
 
 function IndexPage() {
   return (
