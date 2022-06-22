3140cad72897726738cb2d42a9c9b53237fb9a22
Kevin Carrillo <kevincarrilloacosta@gmail.com>
2022-06-22 17:16:54 -0500

    Add ProductCard with Stories and Unit test

---
+++ components/productCard/ProductCard.spec.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import ProductCard from './ProductCard';
+
+const product = {
+    id: 1212,
+    name: 'Mango',
+    category_id: 21,
+    price: 1100,
+    images: ['https://technext.github.io/ogani/img/product/product-6.jpg'],
+  };
+
+  describe('ProductCard component', () => {
+    it('Should render the default component', () => {
+      const mockFavorite = jest.fn();
+      const mockCart = jest.fn();
+      const { getByText } = render(<ProductCard onFavorite={mockFavorite} onCart={mockCart} product={product} />);
+      const productName = getByText('Mango');
+      expect(productName).toBeDefined();
+    });
+
+  it('Should call the event when favorite is selected', () => {
+    const mockFavorite = jest.fn();
+    const mockCart = jest.fn();
+    const { getByTestId } = render(<ProductCard onFavorite={mockFavorite} onCart={mockCart} product={product} />);
+    const button = getByTestId('custom-favorite');
+    fireEvent.click(button);
+    expect(mockFavorite).toHaveBeenCalledTimes(1);    
+  });
+
+  it('Should call the event when cart is selected', () => {
+    const mockFavorite = jest.fn();
+    const mockCart = jest.fn();
+    const { getByTestId } = render(<ProductCard onFavorite={mockFavorite} onCart={mockCart} product={product} />);
+    const button = getByTestId('custom-cart');
+    fireEvent.click(button);
+    expect(mockCart).toHaveBeenCalledTimes(1);    
+  });
+ 
+});
---
+++ components/productCard/ProductCard.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Product } from '../../entities/product.entity';
+import './ProductCard.css'
+
+type Props = {
+  product: Product;
+  onFavorite: (Product) => void;
+  onCart: (Product) => void;
+};
+
+function ProductCard({ product, onFavorite, onCart }: Props) {
+  const handleLike = () => {
+    onFavorite(product);
+  };
+
+  const handleCart = () =>{
+    onCart(product);
+  }
+
+  return (
+    <div className='featured__item'>
+      <div className='featured__item__pic set-bg' style={{ backgroundImage: 'url(' + product.images[0] + ')' }}>
+        <ul className='featured__item__pic__hover'>
+          <li>
+            <button data-testid='custom-favorite' onClick={handleLike}>
+              <i className='fa fa-heart' ></i>
+            </button>
+          </li>
+          <li>
+            <button data-testid='custom-cart' onClick={handleCart}>
+              <i className='fa fa-shopping-cart' ></i>
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className='featured__item__text'>
+        <h6>
+          {product.name}
+        </h6>
+        <h5>${product.price}</h5>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
